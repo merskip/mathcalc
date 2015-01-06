7b782bde891ce803e01c70a873ea70932036b59a
Piotr Merski <merskip@gmail.com>
2015-01-06 12:25:34 +0100

    Poprawki do generowanego pliku Version.hpp w CMake

---
+++ include/Version.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+// Ten plik powinien być generowane przez CMake zaraz przed kompilacją w katalogu budowania.
+// Jeśli z jakiegoś powodu to by nie nastąpiło, zostanie użyty ten,
+// aby nie było błędu kompilacji z powodu brakującego pliku.
+// Ale zostanie zgłoszone ostrzenie dla kompilatora.
+
+#warning "Used an unknown version"
+#define APP_VERSION "UNKNWON"
+#define APP_BUILD_DATE "UNKNOWN"
